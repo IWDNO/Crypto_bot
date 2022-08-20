1287b16ee46533b2ab0a062ab24141dd00d4b8d2
IWDNO <zaharovpavel10133@gmail.com>
2022-08-20 18:58:55 +0300

    v1.0

--- bot.py
+++ bot.py
@@ -264,12 +264,7 @@ def get_message():
     return '!', 200
 
 
-@server.route('/')
-def webhook():
+if __name__ == '__main__':
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
-    return '!', 200
-
-
-if __name__ == '__main__':
     server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
